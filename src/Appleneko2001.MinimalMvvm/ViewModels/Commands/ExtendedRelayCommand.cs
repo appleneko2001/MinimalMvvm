86a0cbf8f300d5e3458a8a321dfe0d0d9c4916cf
appleneko2001 <appleneko2001@outlook.com>
2022-04-12 01:39:58 +0900

    Implement ExtendedRelayCommand (support MayExecute feature)

---
+++ src/Appleneko2001.MinimalMvvm/ViewModels/Commands/ExtendedRelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MinimalMvvm.ViewModels.Commands.Interfaces;
+
+namespace MinimalMvvm.ViewModels.Commands
+{
+    public class ExtendedRelayCommand : RelayCommand, IMayExecuteCommand
+    {
+        private Func<object, bool>? _mayExecute;
+        
+        // ReSharper disable once InconsistentNaming
+        private event EventHandler? _mayExecuteChanged;
+
+        public event EventHandler MayExecuteChanged
+        {
+            add => _mayExecuteChanged += value;
+            remove => _mayExecuteChanged -= value;
+        }
+
+        public ExtendedRelayCommand(Action<object?> execute,
+            Func<object?, bool>? canExecute = null,
+            Func<object?, bool>? mayExecute = null) : base(execute, canExecute)
+        {
+            _mayExecute = mayExecute;
+        }
+        
+        public bool MayExecute(object parameter)
+        {
+            var result = _mayExecute == null || _mayExecute(parameter);
+            return result;
+        }
+        
+        // Call this method to tell AvaloniaUI about this command can be executed at this moment.
+        public void RaiseMayExecuteChanged()
+        {
+            _mayExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ src/Appleneko2001.MinimalMvvm/ViewModels/Commands/Interfaces/IMayExecuteCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MinimalMvvm.ViewModels.Commands.Interfaces
+{
+    public interface IMayExecuteCommand
+    {
+        event EventHandler MayExecuteChanged;
+
+        bool MayExecute(object parameter);
+    }
+}
