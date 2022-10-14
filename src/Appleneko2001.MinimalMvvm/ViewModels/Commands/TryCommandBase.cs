3c3b6389a0084e7ca1631ff89ed6ed2188ee8884
appleneko2001 <appleneko2001@outlook.com>
2022-10-14 18:51:08 +0900

    Add CommandBase with try-catch variant

---
+++ src/Appleneko2001.MinimalMvvm/ViewModels/Commands/TryCommandBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MinimalMvvm.ViewModels.Commands
+{
+    public class TryCommandBase : CommandBase
+    {
+        public override void Execute(object? parameter)
+        {
+            OnExecuteProcedure(parameter);
+        }
+
+        protected virtual void OnExecuteProcedure(object? p)
+        {
+            try
+            {
+                OnTryExecute(p);
+            }
+            catch (Exception e)
+            {
+                var handled = DispatchErrorEventInternal(this, e);
+                if (!handled)
+                    throw;
+            }
+        }
+
+        protected virtual void OnTryExecute(object? p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
